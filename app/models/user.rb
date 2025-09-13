4823e48f19e676e8a3e8480475317c08e5d3b3b6
phanil kumar <phanilkumar@outlook.com>
2025-09-13 07:06:20 +0530

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,35 +7,11 @@ class User < ApplicationRecord
 
   # Method to create user from OAuth data
   def self.from_omniauth(auth)
-    # First, try to find user by provider and uid
-    user = where(provider: auth.provider, uid: auth.uid).first
-    
-    if user
-      return user
-    end
-    
-    # If not found by provider/uid, try to find by email
-    user = where(email: auth.info.email).first
-    
-    if user
-      # Update existing user with OAuth info
-      user.update!(
-        provider: auth.provider,
-        uid: auth.uid,
-        name: auth.info.name,
-        image: auth.info.image
-      )
-      return user
-    end
-    
-    # Create new user
-    create! do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.image = auth.info.image
-      user.provider = auth.provider
-      user.uid = auth.uid
       # Skip email confirmation for OAuth users
       user.skip_confirmation!
     end
