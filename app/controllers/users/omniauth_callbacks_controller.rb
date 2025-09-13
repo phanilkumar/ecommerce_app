3fd8846d60dc3f1295b05cf1e4181d18544623fc
phanil kumar <phanilkumar@outlook.com>
2025-09-13 08:05:59 +0530

    Fix OAuth signup redundancy issue

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,33 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    result = User.from_omniauth(request.env['omniauth.auth'])
+    @user = result[:user]
+    status = result[:status]
+    oauth_source = request.env['omniauth.params']['oauth_source'] # Get the source from OAuth params
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
+      case status
+      when :existing
+        # User already exists with this OAuth provider
+        if oauth_source == 'signup'
+          # User tried to sign up but account already exists - redirect to login
+          flash[:alert] = "An account with this email already exists. Please sign in with Google instead."
+          redirect_to new_user_session_path
+        else
+          # User is signing in normally
+          flash[:notice] = "Welcome back! You're already signed in with Google."
+          sign_in_and_redirect @user, event: :authentication
+        end
+      when :updated
+        # Existing user account was updated with OAuth info
+        flash[:notice] = "Your account has been successfully linked with Google. You can now sign in with Google in the future."
+        sign_in_and_redirect @user, event: :authentication
+      when :new
+        # New user account created
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
+      end
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
          omniauth_providers: [:google_oauth2]
 
   # Method to create user from OAuth data
+  # Returns a hash with :user and :status (:existing, :updated, :new)
   def self.from_omniauth(auth)
     # First, try to find user by provider and uid
     user = where(provider: auth.provider, uid: auth.uid).first
     
     if user
-      return user
+      return { user: user, status: :existing }
     end
     
     # If not found by provider/uid, try to find by email
@@ -25,11 +26,11 @@ class User < ApplicationRecord
         name: auth.info.name,
         image: auth.info.image
       )
-      return user
+      return { user: user, status: :updated }
     end
     
     # Create new user
-    create! do |user|
+    user = create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
@@ -39,5 +40,7 @@ class User < ApplicationRecord
       # Skip email confirmation for OAuth users
       user.skip_confirmation!
     end
+    
+    return { user: user, status: :new }
   end
 end
